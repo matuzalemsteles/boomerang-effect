2b59f4de9a27126513de1139dce08eb14d12efa4
Matuzalém Teles <matuzalemteles@gmail.com>
2018-09-09 02:37:12 -0300

    Add coveralls support

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
 	verbose: true,
 	collectCoverage: true,
+	"coverageReporters": [
+		"json",
+		"lcov"
+	],
 	coverageDirectory: 'coverage',
 	snapshotSerializers: [
 		"metal-jest-serializer",
